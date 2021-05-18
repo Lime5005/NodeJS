d8a98a5a2c5ce358f46e69b8f807b9ed6faf3747
Lime5005 <zlp5005@gmail.com>
2021-05-18 14:57:17 +0200

    add database

--- express/util/database.js
+++ express/util/database.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('node_complete', 'root', 'Lime5005&', { dialect: 'mysql', host: 'localhost' })
+const sequelize = new Sequelize('node_complete', 'root', '', { dialect: 'mysql', host: 'localhost' })
 
 module.exports = sequelize
